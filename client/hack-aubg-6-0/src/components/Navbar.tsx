6c23c5643239e6318cd17d42b8778146820d64e1
Pepkin <gospodinov.petko@icloud.com>
2024-03-29 23:13:45 +0200

    yes

--- client/hack-aubg-6-0/src/components/Navbar.tsx
+++ client/hack-aubg-6-0/src/components/Navbar.tsx
@@ -1,5 +1,17 @@
-import React from "react";
+import React from 'react';
 
 export default function Navbar() {
-  return <div className="bg-blue-700">Navbar</div>;
+  return (
+    <div className="flex justify-center items-stretch bg-primary">
+      <div className="px-3">
+        <h2>One</h2>
+      </div>
+      <div className="px-3">
+        <h2>two</h2>
+      </div>
+      <div className="px-3">
+        <h2>three</h2>
+      </div>
+    </div>
+  );
 }
