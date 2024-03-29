18e81e4e4e2cec64b4c34946a9a36f2bf89d3b11
AlexanderTsoiGeorgiev <georgiev.alexander03@gmail.com>
2024-03-30 01:19:18 +0200

    Fixed Main Centering

--- client/hack-aubg-6-0/src/app/layout.tsx
+++ client/hack-aubg-6-0/src/app/layout.tsx
@@ -20,7 +20,9 @@ export default function RootLayout({
     <html lang="en">
       <body className="flex flex-col min-h-screen">
         <Navbar />
-        {children}
+        <div className="flex flex-auto justify-center">
+          {children}
+        </div>
         <Footer />
       </body>
     </html>
--- client/hack-aubg-6-0/src/app/page.tsx
+++ client/hack-aubg-6-0/src/app/page.tsx
@@ -2,11 +2,11 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-auto justify-center">
+    
       <div className=" ">
         <h1>Hello World</h1>
       </div>
-    </div>
+    
   );
 }
 
