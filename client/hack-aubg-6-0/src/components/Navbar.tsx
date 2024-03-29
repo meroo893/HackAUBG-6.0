7c397e8ecbd145e1ed2ab5308ae7623da19ef06c
Pepkin <gospodinov.petko@icloud.com>
2024-03-29 23:44:59 +0200

    it works

--- client/hack-aubg-6-0/src/components/Navbar.tsx
+++ client/hack-aubg-6-0/src/components/Navbar.tsx
@@ -1,4 +1,16 @@
+'use client';
+
 import React from 'react';
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+} from '@/components/ui/navigation-menu';
 
 export default function Navbar() {
   return (
@@ -6,18 +18,29 @@ export default function Navbar() {
       <div className="flex bg-blue-200 justify-between items-start w-4/5">
         <div>ikonka</div>
         <div className="flex flex-row">
-          <div className="flex px-3 py-2 border border-transparent rounded-md bg-orange-500 text-white cursor-pointer hover:bg-orange-600 m-2">
-            <h2>One</h2>
-          </div>
-          <div className="flex px-3 py-2 border border-transparent rounded-md bg-orange-500 text-white cursor-pointer hover:bg-orange-600 m-2">
-            <h2>Two</h2>
-          </div>
-          <div className="flex px-3 py-2 border border-transparent rounded-md bg-orange-500 text-white cursor-pointer hover:bg-orange-600 m-2">
-            <h2>Three</h2>
-          </div>
+          <NavigationMenu>
+            <NavigationMenuList>
+              <NavigationMenuItem>
+                <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+                <NavigationMenuContent>
+                  <NavigationMenuLink>Link</NavigationMenuLink>
+                </NavigationMenuContent>
+              </NavigationMenuItem>
+            </NavigationMenuList>
+          </NavigationMenu>
         </div>
         <div>login</div>
       </div>
     </div>
   );
 }
+
+// <div className="flex px-3 py-2 border border-transparent rounded-md bg-orange-500 text-white cursor-pointer hover:bg-orange-600 m-2">
+//   <h2>One</h2>
+// </div>
+// <div className="flex px-3 py-2 border border-transparent rounded-md bg-orange-500 text-white cursor-pointer hover:bg-orange-600 m-2">
+//   <h2>Two</h2>
+// </div>
+// <div className="flex px-3 py-2 border border-transparent rounded-md bg-orange-500 text-white cursor-pointer hover:bg-orange-600 m-2">
+//   <h2>Three</h2>
+// </div>
