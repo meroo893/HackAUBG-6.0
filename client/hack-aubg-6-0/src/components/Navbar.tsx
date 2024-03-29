b4f05179706e5bd65d91e0d992d20da780d91a63
Pepkin <gospodinov.petko@icloud.com>
2024-03-29 23:32:12 +0200

    good yes

--- client/hack-aubg-6-0/src/components/Navbar.tsx
+++ client/hack-aubg-6-0/src/components/Navbar.tsx
@@ -2,15 +2,21 @@ import React from 'react';
 
 export default function Navbar() {
   return (
-    <div className="flex justify-center items-stretch bg-primary">
-      <div className="px-3">
-        <h2>One</h2>
-      </div>
-      <div className="px-3">
-        <h2>two</h2>
-      </div>
-      <div className="px-3">
-        <h2>three</h2>
+    <div className="flex justify-center items- bg-primary">
+      <div className="flex bg-blue-200 justify-between items-start w-4/5">
+        <div>ikonka</div>
+        <div className="flex flex-row">
+          <div className="flex px-3 py-2 border border-transparent rounded-md bg-orange-500 text-white cursor-pointer hover:bg-orange-600 m-2">
+            <h2>One</h2>
+          </div>
+          <div className="flex px-3 py-2 border border-transparent rounded-md bg-orange-500 text-white cursor-pointer hover:bg-orange-600 m-2">
+            <h2>Two</h2>
+          </div>
+          <div className="flex px-3 py-2 border border-transparent rounded-md bg-orange-500 text-white cursor-pointer hover:bg-orange-600 m-2">
+            <h2>Three</h2>
+          </div>
+        </div>
+        <div>login</div>
       </div>
     </div>
   );
