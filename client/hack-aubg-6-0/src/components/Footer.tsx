743b5a64ba29c1a6b38b76b7dc984d57340f81c2
AlexanderTsoiGeorgiev <georgiev.alexander03@gmail.com>
2024-03-29 23:34:13 +0200

    Shadcn Install commit

--- client/hack-aubg-6-0/src/components/Footer.tsx
+++ client/hack-aubg-6-0/src/components/Footer.tsx
@@ -1,3 +1,7 @@
 export default function Footer() {
-  return <div className="bg-red-500">Footer</div>;
+  return (
+    <div className="bg-red-500 min-w-full">
+      <div className="min-w-[1024px] m-auto bg-slate-800">Footer</div>
+    </div>
+  );
 }
--- client/hack-aubg-6-0/src/components/Navbar.tsx
+++ client/hack-aubg-6-0/src/components/Navbar.tsx
@@ -1,8 +1,20 @@
-import React from 'react';
+import React from "react";
+
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
 
 export default function Navbar() {
   return (
     <div className="flex justify-center items-stretch bg-primary">
+      <div></div>
+      <div className=""></div>
       <div className="px-3">
         <h2>One</h2>
       </div>
