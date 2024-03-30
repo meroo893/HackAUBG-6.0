62f12bb3dafd2647d3611716a0c7f273367c47c5
Pepkin <gospodinov.petko@icloud.com>
2024-03-30 18:46:29 +0200

    done

--- client/hack-aubg-6-0/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-
-export default function Home() {
-  return (
-    <div className="min-w-[1024px]">
-      <h1 className="m-auto">Hello World</h1>
-    </div>
-  );
-}
