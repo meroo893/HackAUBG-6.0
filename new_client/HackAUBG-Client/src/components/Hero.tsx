aede4c38c52a14c9ce020562b9fc5f66643daa9f
AlexanderTsoiGeorgiev <georgiev.alexander03@gmail.com>
2024-03-30 10:00:22 +0200

    Added Jumbotron to Hero

--- new_client/HackAUBG-Client/src/components/Hero.tsx
+++ new_client/HackAUBG-Client/src/components/Hero.tsx
@@ -1,13 +1,17 @@
+import HeroJumbotron from "./HeroJumbotron";
+
 //Add hero
 export default function Hero() {
   return (
-    <div className="flex flex-col justify-center border-2 border-border rounded-md bg-red-700 min-h-32 mt-4">
-      <div className="flex justify-center items-center bg-blue-400">
-        <span className="">Hero Title</span>
+    <>
+      <div className="flex flex-col justify-center border-2 border-border rounded-md bg-red-700 min-h-32 mt-4">
+        <div className="flex justify-center items-center bg-blue-400">
+          <span className="min-h-12">Hero Title</span>
+        </div>
+        <div className="flex justify-center items-center flex-auto">
+          <HeroJumbotron />
+        </div>
       </div>
-      <div className="flex justify-center items-center bg-orange-700 flex-auto">
-        Hero CTA
-      </div>
-    </div>
+    </>
   );
 }
