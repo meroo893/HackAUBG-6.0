2e0555cb937268ded796a42837c71a74836f107d
Pepkin <gospodinov.petko@icloud.com>
2024-03-31 11:05:19 +0300

    no

--- new_client/HackAUBG-Client/src/components/Navigation/Navbar.tsx
+++ new_client/HackAUBG-Client/src/components/Navigation/Navbar.tsx
@@ -5,27 +5,13 @@ import {
   NavigationMenuLink,
   NavigationMenuList,
   NavigationMenuTrigger,
-} from "../ui/navigation-menu";
-import { Link } from "react-router-dom";
+} from '../ui/navigation-menu';
+import { Link } from 'react-router-dom';
 
 export default function Navbar() {
   return (
     <NavigationMenu>
       <NavigationMenuList>
-        <NavigationMenuItem>
-          <NavigationMenuTrigger className="!bg-primary hover:!text-foreground hover:!bg-secondary text-black focus:!text-foreground">
-            Users
-          </NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <ul className="flex justify-around w-[320px] list-none items-center">
-              <Link to="/users" className="text-lg">
-                See All Users
-              </Link>
-            </ul>
-
-            <NavigationMenuLink></NavigationMenuLink>
-          </NavigationMenuContent>
-        </NavigationMenuItem>
         <NavigationMenuItem>
           <NavigationMenuTrigger className="!bg-primary hover:!text-foreground hover:!bg-secondary text-black focus:!text-foreground">
             Add Data
